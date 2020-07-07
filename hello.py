187b9c74d57c728faf36eefb7b5f54d6b1b9cb51
Junkes887 <henriquejunkes887@gmail.com>
2020-07-06 22:44:48 -0300

    test 01

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
-print("teste")
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+
+def cria_audio(audio):
+    tts = gTTS(audio,lang='pt-br', show=False)
+ 
+    tts.save('audios/hello.mp3')
+    print("Estou aprendendo o que você disse...")
+
+    playsound('audios/hello.mp3')
+def ouvir_microfone():
+    microfone = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        audio = microfone.listen(source)
+        frase = microfone.recognize_google(audio,language='pt-BR')
+  
+    try:
+        print("Você disse: " + frase.capitalize())
+        if frase.capitalize() == "Meu nome é eder":
+            print("Viado Detectado")
+    except sr.UnkownValueError:
+        print("Não entendi")
+frase = ouvir_microfone()
+cria_audio("Teste")
